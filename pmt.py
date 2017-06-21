61e37763eb026f4fddd33bfcc2dfedd24a910441
QDS-PHUCDO <phuc.do@quodisys.com>
2017-06-21 14:15:48 +0700

    Update test 1

--- pmt.py
+++ pmt.py
@@ -60,9 +60,11 @@ reload(sys)
 app = Flask(__name__)# pylint: disable=invalid-name
 app.config.from_pyfile('src/py/config.py')
 #Config release with sentry
-
-app.config['SENTRY_CONFIG'] = { 'include_paths': [app.config['SENTRY_PROJECT']],\
-    'release': os.environ['HEROKU_SLUG_COMMIT']}
+if os.environ.get('RELEASE_SENTRY') == "PRODUCTION":
+    app.config['SENTRY_CONFIG'] = {
+        'include_paths': [app.config['SENTRY_PROJECT']],
+        'release': os.environ['HEROKU_SLUG_COMMIT']
+    }
 
 # Import and run Sentry
 SENTRY = Sentry()
